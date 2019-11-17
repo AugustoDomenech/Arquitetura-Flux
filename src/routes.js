bb64f1491ecb0e956d8643c83cf0ab31621be40f
Augusto Domenech <augustodomenech@gmail.com>
2019-11-17 16:58:05 -0300

    Configurado as rotas da aplicação

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+// import { Container } from './styles';
+
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+
+function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/cart" component={Cart} />
+    </Switch>
+  );
+}
+
+export default Routes;
